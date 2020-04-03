3a60c8c12e87ea4cf966507ff1e023ed636edb5d
国庆 赵 <1164442003@qq.com>
2020-04-03 13:59:02 +0800

    删除旧版

--- CQ_SDK/AuthCode.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-inline
-namespace CQ {
-	// 获取调用api所需的AuthCode
-	int getAuthCode();
-}
--- CQ_SDK/DEBUG.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Windows.h>
-
-#define CQTry __try{
-
-#define CQTryEnd(evename,msg) }__except(dump(GetExceptionInformation(),#evename,#msg)){}
-
-int dump(EXCEPTION_POINTERS* ep, const char*evename, const char*msg);
