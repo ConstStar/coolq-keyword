b1a3f877fff35abf52e0b1a51411543a490224b7
国庆 赵 <1164442003@qq.com>
2020-04-19 16:02:55 +0800

    2.20

--- src/mycq.hpp
+++ src/mycq.hpp
@@ -1,7 +1,7 @@
 #include <cqcppsdk/cqcppsdk.h>
 #pragma once
 #define APPNAME "群关键词监控"
-#define VERSION_ID 25
+#define VERSION_ID 26
 // namespace mycq::logging {
 //     void info(std::string word) {
 //         cq::logging::info(APPNAME, word);
